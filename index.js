f79f66b1d055d243c02b67005a993303a205b98f
Snoopyo <ws.emallates@gmail.com>
2016-12-29 10:10:45 +0400

    Added reconnect on error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
-var _ = require('lodash');
-var utils = require('./lib/utils');
+var _ = require('./lib/utils');
 var amqp = require('amqplib/callback_api');
 
 module.exports = rmqManager
@@ -8,16 +7,26 @@ module.exports = rmqManager
 function rmqManager(opts) { opts = opts || {};
 	var _s = this;
 	if (!(_s instanceof rmqManager)) { return new rmqManager(opts); }
-	_s.options={
-		noAct: (opts.noAck !== undefined) ? opts.noAck : true,
-		protocol:opts.host || 'amqp:',
-		host:opts.host || opts.url || opts.ip || '127.0.0.1',
-		write:opts.canPublish || false,
-		read:opts.canConsume || false,
-		defQ:opts.defaultQ || 'zglog-queue-default'
-	};
-	_s.options.url = opts.url || (_s.options.protocol+'//'+_s.options.host);
-	_s.options.Qs = utils.getQs(opts.Qs || opts.qs || opts.queues);
+
+
+	_s.options = _.assignIn({
+		/* optional opts */
+		// port:5672,
+		// protocol:'amqp',
+		// hostname:'127.0.0.1',
+		
+		username:'admin',
+		password:'admin',
+
+		noAct: false,
+		retryTime:5000,
+		canPublish:true,
+		canConsume:true,
+		defaultQ:'zglog-queue-default'
+	}, opts);
+
+	_s.options.Qs = _.getQs(opts.Qs || opts.qs || opts.queues);
+
 	_s.init();
 	return _s;
 }
@@ -28,14 +37,24 @@ _.extend(
 	require('./lib/publisher')
 );
 
-rmqManager.prototype.init = function getNewChanel(callback){
-	var _s = this;
-	if(_s.conn) return callback(null, true);
-	amqp.connect(_s.options.url, function(err, conn){
-		if(err) throw err;
-		_s.conn = conn;
-		if(callback) callback(null, true);
-	});
+rmqManager.prototype.init = function (callback){
+	try{
+		var _s = this;
+		if(_s.conn) return callback(null, true);
+		amqp.connect(_s.options, function(err, conn){
+			if(err) throw err;
+			_s.conn = conn;
+			if(_s.timer) clearInterval(_s.timer);
+
+			_s.conn.on("error", function (err) {
+				_s.timer = setInterval(_s.init, _.options.retryTime);
+			})
+			// "error", function(e) { log(e); setInterval(restart, 5000); 
+			if(callback) callback(null, true);
+		});		
+	}catch(exp){
+		if(callback) callback(exp);
+	}
 }
 rmqManager.prototype.getChanel = function getNewChanel(q, callback){
 	this.conn.createChannel(function(err, chanel){
--- test/basic.js
+++ test/basic.js
@@ -1,15 +1,15 @@
 var manager = require('../');
 manager = new manager({
-	defaultQ:"inputs",
-	// noAck:false,
+	defaultQ:"zlogjs-rabbitmq-defQ",
+	noAck:true,
 	canPublish:true,
 	canConsume:true,
 	qs:['normal','errors']//'normal' || ['normal','errors'] || {normal:1, errors:1}	
 });
-console.log(manager);
+// console.log(manager);
 setTimeout(function(){
 	manager.publish("Message string", /*'testQ',*/ console.log);
-	manager.consume(function(err, msg/*, done*/){
+	manager.consume(function(err, msg, done){
 		console.log('err', err);
 		console.log('msg', msg);
 		// done();
